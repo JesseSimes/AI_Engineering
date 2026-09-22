24e743c75613d00c43b4acc00da7679ab623e49a
Jesse Priyanshu Simes <jessesimes13@gmail.com>
2026-09-22 17:44:52 +0530

    First push

---
+++ Python/main.py
@@ -0,0 +1,47 @@
+# num = int(input("Enter a number: "))
+
+# if (num%2 == 0):
+#     print(f"{num} is even")
+
+# else:
+#     print(f"{num} is not even")
+
+# n = int(input())
+
+# for i in range (1, 11, 1):
+#     print(f"{i} * {n} = {i * n}")
+
+# n = int(input())
+# sum = 0
+
+# # for i in range (1, n+1, 1):
+# #     print("Hello World")
+
+# # for i in range (1, n+1, 1):
+# #     sum += i
+
+# # print(sum)
+
+# sumOdd = 0
+# sumEven = 0
+
+# for i in range (1, n+1, 1):
+#     if(i%2==0):
+#         sumEven += i
+#     else:
+#         sumOdd += i
+
+# print(f"Sum of even numbers is {sumEven}")
+# print(f"Sum of Odd numbers is {sumOdd}")
+
+
+
+# n = int(input())
+# rev = 0
+
+# while (n > 0):
+#     rev = rev * 10 + n % 10
+
+#     n //= 10
+
+# print(rev)
