a7ee78a90c2e4e252f74a6c9af5ec85ce57115b9
Jesse Priyanshu Simes <jessesimes13@gmail.com>
2026-09-25 13:54:49 +0530

    NumPy Array and Array functions and operations for 1D and 2D Array

--- Python/main.py
+++ Python/main.py
@@ -45,3 +45,77 @@
 #     n //= 10
 
 # print(rev)
+
+
+# l=[1,2,3,-4,-5,-6,9,10]
+
+# for i in range(len(l)):
+#     if(l[i] < 0):
+#         print(f"{l[i]} is negative")
+#     else:
+#         print(f"{l[i]} is positive")
+
+
+# l=[52,79,60,34,22,13,65]
+
+# a=0
+
+# for i in range(len(l)):
+#     a += l[i]
+
+# print(f"Average: {a / len(l)}")
+
+
+# l=[52,79,60,34,22,13,65]
+# g =0 
+
+# for i in range(len(l)):
+#     if (l[i] > g):
+#         g = l[i]
+
+# print(g)
+
+# l=[52,79,60,34,22,13,65]
+
+# largest=0
+# second=0
+# index=0
+
+# for i in range(len(l)):
+#     if(l[i]>largest):
+#         largest=l[i]
+#     elif(l[i]>second and l[i]<largest):
+#         second=l[i]
+#         index = i
+
+# print(second)
+# print(index)
+
+
+
+# l=[1,2,3,4,5]
+
+# for i in range(len(l)-1):
+#     if(l[i]<l[i+1]):
+#         continue
+#     else:
+#         print("Not sorted")
+#         break
+# else:
+#     print("Sorted")
+
+
+# d1={10:100,20:200,30:300}
+# d2={40:400,50:500,60:600}
+
+# for i in d2:
+#     d1[i] = d2[i]
+
+
+def add (*args):
+    sum = 0
+    for i in args:
+        sum += i
+    print(sum)
+
+add(1,2,3,4,5,6,7,8,9,10)
